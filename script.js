d9a4b3e32ec8fd00beffbfb39032d0a59e5f0e6d
itzKamran <siddiquekamran72@gmail.com>
2026-03-04 03:09:48 +0530

    Update script.js
    Signed-off-by: itzKamran <siddiquekamran72@gmail.com>

--- script.js
+++ script.js
@@ -235,9 +235,7 @@ const facultyData = {
       name:"Cs-603 Computer Network",
       imp:"https://docs.google.com/document/d/1HkHJutAF6u_hIj4-pe-TjPXSu3kDORZV/edit?usp=drive_link&ouid=115459576305560944580&rtpof=true&sd=true",
       pdf:"https://drive.google.com/drive/folders/1xt_s8ZnV5CRZGVRSMsSON-iyf204kz38?usp=drive_link",
-      classroom::,
-      videos:["playlist?list=PLqleLpAMfxGCUpDRFUnLKeDrgBsPOwTQK"]
-    },
+      classroom:"https://docs.google.com/document/d/1zp9tR83A_Q5qr2MnMkXbvMETMLnrFvBy/edit?usp=drive_link&ouid=102998916905628950928&rtpof=true&sd=true":[
     {
       name:"Cs-604 Theoretical Computer Science",
       imp:"https://docs.google.com/document/d/1HkHJutAF6u_hIj4-pe-TjPXSu3kDORZV/edit?usp=drive_link&ouid=115459576305560944580&rtpof=true&sd=true",
@@ -257,7 +255,8 @@ const facultyData = {
       imp:"https://docs.google.com/document/d/1HkHJutAF6u_hIj4-pe-TjPXSu3kDORZV/edit?usp=drive_link&ouid=115459576305560944580&rtpof=true&sd=true",
       pdf:"https://drive.google.com/drive/folders/1CPY07muZueW00m8zvk-584VBy7eQLpQT?usp=drive_link",
       https://docs.google.com/document/d/1zp9tR83A_Q5qr2MnMkXbvMETMLnrFvBy/edit?usp=drive_link&ouid=102998916905628950928&rtpof=true&sd=trueclassroom:"#",
-     https://docs.google.com/document/d/1zp9tR83rtpof=true&sd=true";   videos:["playlist?list=PLfqMhTWNBTe3LtFWcvwpqTkUSlB32kJop","xTtL8E4LzTQ"] },
+      videos:["playlist?list=PLfqMhTWNBTe3LtFWcvwpqTkUSlB32kJop","xTtL8E4LzTQ"]
+    }.
   ],
 },
 "BCA":{
@@ -1008,6 +1007,7 @@ alert("Prompt Copied ✅");
 window.open("https://gemini.google.com/","_blank");
 }
 }
+
 
 
 
